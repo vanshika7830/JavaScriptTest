21a5c9610dada76ecacd2e28dcd52c3f187cba51
unknown <vanshikaguptaaa2005@gmail.com>
2026-02-23 10:39:24 +0530

    Updated problem 2

--- Week4/02_problem.js
+++ Week4/02_problem.js
@@ -1,27 +1,52 @@
 const express = require("express");
 const app = express();
+
 app.use(express.json());
 
-let users = [];
+let products = [
+  { id: 1, name: "Mobile", price: 20000 },
+  { id: 2, name: "Tablet", price: 15000 }
+];
 
-
-app.use((req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-});
-
-
-function validateUser(req, res, next) {
-  const { name, age } = req.body;
-  if (!name || !age) {
-    return res.status(400).json({ message: "Name and age required" });
-  }
-  next();
+function logger(req, res, next) {
+  console.log("Middleware called");
+  console.log(`Api Info : ${req.method} ${req.url}`);
+  next(); 
 }
 
-app.post("/users", validateUser, (req, res) => {
-  users.push(req.body);
-  res.json({ message: "User added", users });
+app.use(logger);
+
+function validateProduct(req, res, next) {
+  const { name, price } = req.body;
+
+  if (!name || !price) {
+    return res.status(400).json({
+      message: "Name and price are required"
+    });
+  }
+
+  next(); 
+}
+
+app.get("/products", (req, res) => {
+  res.json(products);
 });
 
-app.listen(3000, () => console.log("Server running"));
+app.post("/products", validateProduct, (req, res) => {
+  const { name, price } = req.body;
+
+  const newProduct = {
+    id: products.length + 1,
+    name,
+    price
+  };
+
+  products.push(newProduct);
+
+  res.status(201).json(newProduct);
+});
+
+
+app.listen(3010, () => {
+  console.log("Server running on port 3010");
+});
