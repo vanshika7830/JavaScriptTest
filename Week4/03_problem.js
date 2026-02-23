df1e7c58830bb8d46be5436c4b968cca3ac22e6f
unknown <vanshikaguptaaa2005@gmail.com>
2026-02-23 10:22:37 +0530

    Probelm3

---
+++ Week4/03_problem.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+app.use(cookieParser());
+
+// Visit route
+app.get("/visit", (req, res) => {
+  let count = req.cookies.visitCount;
+
+  if (!count) {
+    count = 1;
+  } else {
+    count = parseInt(count) + 1;
+  }
+
+  res.cookie("visitCount", count);
+  res.json({ message: `You have visited this page ${count} times` });
+});
+
+// Reset route
+app.get("/reset", (req, res) => {
+  res.clearCookie("visitCount");
+  res.json({ message: "Visit count reset" });
+});
+
+app.listen(3000, () => console.log("Server running"));
